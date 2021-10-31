9d79f44f1afb0357a03ef270592ad1b66ea621a0
Chris Ahrens <christopher00ahrens@gmail.com>
2021-10-31 15:18:41 -0700

    Boat (non-functional)
    Added code from provided code, will revert to this later in case anything truly awful is added

--- threads/Boat.java
+++ threads/Boat.java
@@ -4,6 +4,11 @@ import nachos.ag.BoatGrader;
 public class Boat
 {
     static BoatGrader bg;
+    static boolean notDone;
+    static boolean boatIsOnOahu;
+    static boolean rowersToMolokai;
+    static Lock lock;
+    static int childrenOnBoat;
     
     public static void selfTest()
     {
@@ -26,6 +31,10 @@ public class Boat
 	bg = b;
 
 	// Instantiate global variables here
+	notDone = true;
+	boatIsOnOahu = true;
+	lock = new Lock();
+	childrenOnBoat = 0;
 	
 	// Create threads here. See section 3.4 of the Nachos for Java
 	// Walkthrough linked from the projects page.
@@ -38,7 +47,32 @@ public class Boat
         KThread t = new KThread(r);
         t.setName("Sample Boat Thread");
         t.fork();
-
+    
+	    Runnable r_child = new Runnable() {
+	    	public void run() {
+	    		ChildItinerary();
+	    	}
+	    }; //r_child
+	    
+	    Runnable r_adult = new Runnable() {
+		   public void run() {
+			   AdultItinerary();
+		   }
+	   	}; //r_adult
+	   
+	   	//Spawn Child threads
+	   	for(int i = 0; i < children; i++) {
+		   new KThread(r_child).setName("Child" + Integer.toString(i+1)).fork();
+	   	}
+	   	//Spawn Adult threads
+	   	for(int i = 0; i < adults; i++) {
+	   		new KThread(r_adult).setName("Adult" + Integer.toString(i+1)).fork();
+	   	}
+	   	
+	   	//hold thread while grader runs
+	   	while(notDone)
+	   			KThread.yield();
+	   	
     }
 
     static void AdultItinerary()
@@ -49,10 +83,73 @@ public class Boat
 	       bg.AdultRowToMolokai();
 	   indicates that an adult has rowed the boat across to Molokai
 	*/
+    	lock.acquire();
+    	
+    	while(notDone) {
+    		
+    	}
     }
 
     static void ChildItinerary()
     {
+    	lock.acquire();
+    	
+    	while(notDone) {
+    		while(!boatIsOnOahu) {
+    			//code
+    		}
+    		
+    		//If First to boat, child is a passenger and waits for a rower
+    		if(childrenOnBoat == 0) {
+    			childrenOnBoat++;
+    			//code to row to Molokai:
+    			
+    			
+    			//sleeps
+    			
+    			//Woken up by adult to ferry back to Oahu
+    			bg.ChildRowToOahu();
+    			boatIsOnOahu = true;
+    			//more code
+    			
+    			childrenOnBoat = 0;
+    			//more code
+    			
+    		}
+    		
+    		//If second to boat, child is a rower
+    		else if(childrenOnBoat == 1) {
+    			//two children always bring the boat back from Oahu and check
+    			//if they are done before returning to Oahu for an adult
+    			childrenOnBoat++;
+    			bg.ChildRowToMolokai();
+    			bg.ChildRideToMolokai();
+    			boatIsOnOahu = false;
+    			
+    			//code
+    			
+    			//check if done
+    			//NEED TO CREATE TRACKER FOR NUMBER OF RIDERS PASSED TO MOLOKAI
+    			if(notDone /*REPLACE THIS*/) {
+    				//set terminal bool to false to end all loops and return
+    				notDone = false;
+    				return;
+    			} //boat terminates after this if statement
+    			
+    			//else not done, so send one back to Oahu
+    			else {
+    				bg.ChildRowToOahu();
+    				//code for waking
+    				
+    				childrenOnBoat = 0;
+    				boatIsOnOahu = true;
+    				
+    				//code for sleep
+    			}
+    			
+    		}
+    		
+    	}
     }
 
     static void SampleItinerary()
