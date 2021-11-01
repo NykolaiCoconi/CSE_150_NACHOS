516781be0540919e19d4f88bd1aeb207640d52b1
Chris Ahrens <christopher00ahrens@gmail.com>
2021-10-31 19:04:20 -0700

    added boat self test

--- threads/Boat.java
+++ threads/Boat.java
@@ -1,5 +1,6 @@
 package nachos.threads;
 import nachos.ag.BoatGrader;
+import java.util.LinkedList;
 
 public class Boat
 {
@@ -9,6 +10,28 @@ public class Boat
     static boolean rowersToMolokai;
     static Lock lock;
     static int childrenOnBoat;
+    static LinkedList<KThread> waitingQueue;
+    
+    //Condition variables
+    static Condition adultOnOahu;
+    static Condition adultOnMolokai;
+    static Condition childOnOahu;
+    static Condition childOnMolokai;
+    static Condition onBoat;
+
+    //Our global variables:
+    static int initialChildren;
+    static int initialAdults;
+    
+    static int childrenOnOahu;
+    static int adultsOnOahu;
+    static int childrenOnMolokai;
+    static int adultsOnMolokai;
+    //NOTE: these are not used by the threads, they will store a local variable
+    // with last known number of people on each island!
+    
+    static int boatLocation; // 0 = Oahu, 1 = Molokai
+    
     
     public static void selfTest()
     {
@@ -36,6 +59,26 @@ public class Boat
 	lock = new Lock();
 	childrenOnBoat = 0;
 	
+	initialAdults = adults;
+	initialChildren = children;
+	
+	childrenOnOahu = initialChildren;
+	adultsOnOahu = initialAdults;
+	childrenOnMolokai = 0;
+	adultsOnMolokai = 0;
+
+    adultOnOahu = new Condition(lock);
+    adultOnMolokai = new Condition(lock);
+    childOnOahu = new Condition(lock);
+    childOnMolokai = new Condition(lock);
+    onBoat = new Condition(lock);
+    
+    boatLocation = 0; //boat starts on Oahu, otherwise literally nothing works
+    //not needed if just using boatIsOnOahu
+    
+    waitingQueue = new LinkedList<KThread>();
+	
+	
 	// Create threads here. See section 3.4 of the Nachos for Java
 	// Walkthrough linked from the projects page.
 
@@ -43,35 +86,35 @@ public class Boat
 	    public void run() {
                 SampleItinerary();
             }
-        };
-        KThread t = new KThread(r);
-        t.setName("Sample Boat Thread");
-        t.fork();
+    };
+    KThread t = new KThread(r);
+    t.setName("Sample Boat Thread");
+    t.fork();
+
+    Runnable r_child = new Runnable() {
+    	public void run() {
+    		ChildItinerary();
+    	}
+    }; //r_child
     
-	    Runnable r_child = new Runnable() {
-	    	public void run() {
-	    		ChildItinerary();
-	    	}
-	    }; //r_child
-	    
-	    Runnable r_adult = new Runnable() {
-		   public void run() {
-			   AdultItinerary();
-		   }
-	   	}; //r_adult
-	   
-	   	//Spawn Child threads
-	   	for(int i = 0; i < children; i++) {
-		   new KThread(r_child).setName("Child" + Integer.toString(i+1)).fork();
-	   	}
-	   	//Spawn Adult threads
-	   	for(int i = 0; i < adults; i++) {
-	   		new KThread(r_adult).setName("Adult" + Integer.toString(i+1)).fork();
-	   	}
-	   	
-	   	//hold thread while grader runs
-	   	while(notDone)
-	   			KThread.yield();
+    Runnable r_adult = new Runnable() {
+	   public void run() {
+		   AdultItinerary();
+	   }
+   	}; //r_adult
+   
+   	//Spawn Child threads
+   	for(int i = 0; i < children; i++) {
+	   new KThread(r_child).setName("Child" + Integer.toString(i+1)).fork();
+   	}
+   	//Spawn Adult threads
+   	for(int i = 0; i < adults; i++) {
+   		new KThread(r_adult).setName("Adult" + Integer.toString(i+1)).fork();
+   	}
+   	
+   	//hold thread while grader runs
+   	while(notDone)
+   			KThread.yield();
 	   	
     }
 
@@ -86,26 +129,39 @@ public class Boat
     	lock.acquire();
     	
     	while(notDone) {
+    		while(!boatIsOnOahu || childrenOnOahu >=2) {
+    			adultOnOahu.sleep();
+    		}
+    		// Row to Molokai!
+    		adultsOnOahu--;
+    		bg.AdultRowToMolokai();
+    		adultsOnMolokai++;
+    		boatIsOnOahu = false;
     		
     	}
     }
 
     static void ChildItinerary()
     {
+    	boolean onOahu = true;
     	lock.acquire();
     	
     	while(notDone) {
     		while(!boatIsOnOahu) {
     			//code
+    			if(onOahu)
+    				childOnOahu.sleep();
     		}
     		
     		//If First to boat, child is a passenger and waits for a rower
     		if(childrenOnBoat == 0) {
     			childrenOnBoat++;
     			//code to row to Molokai:
-    			
+    			childOnOahu.wake();
     			
     			//sleeps
+    			onOahu = false;
+    			childOnMolokai.sleep();
     			
     			//Woken up by adult to ferry back to Oahu
     			bg.ChildRowToOahu();
@@ -114,6 +170,7 @@ public class Boat
     			
     			childrenOnBoat = 0;
     			//more code
+    			childOnOahu.sleep();
     			
     		}
     		
@@ -130,7 +187,7 @@ public class Boat
     			
     			//check if done
     			//NEED TO CREATE TRACKER FOR NUMBER OF RIDERS PASSED TO MOLOKAI
-    			if(notDone /*REPLACE THIS*/) {
+    			if(initialAdults == adultsOnOahu && initialChildren == childrenOnOahu) {
     				//set terminal bool to false to end all loops and return
     				notDone = false;
     				return;
@@ -145,6 +202,7 @@ public class Boat
     				boatIsOnOahu = true;
     				
     				//code for sleep
+    				childOnOahu.sleep();
     			}
     			
     		}
